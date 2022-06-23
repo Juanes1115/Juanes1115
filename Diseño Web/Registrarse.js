dfce60604b42f86c0391b740652f22568b022306
Juanes1115 <99339676+Juanes1115@users.noreply.github.com>
2022-06-23 07:36:34 -0500

    Add files via upload

---
+++ Diseño Web/Registrarse.js
@@ -0,0 +1,20 @@
+function Registrarse(){
+    var Nombre = document.getElementById("Nombre").value;
+    var Apellido = document.getElementById("Apellido").value;
+    var Correo = document.getElementById("Correo").value;
+    var Contraseña = document.getElementById("Contraseña").value;
+    
+
+if (Nombre=="")
+    alert("Digite un nombre.")
+
+if (Apellido=="")
+    alert("Digite su apellido.")
+
+if (Correo=="")
+    alert("Escriba su correo electronico.")
+    
+if (Contraseña=="")
+    alert("Digite su contraseña.")
+}
+
